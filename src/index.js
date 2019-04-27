21f346d7677ef00e0043751365cca85ce4b402da
akalenda <akalenda@users.noreply.github.com>
2019-04-27 10:48:43 -0700

    Switched to use of koa-static for serving static, public files.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa');
-const KoaRouter = require('koa-router');
 const KoaLogger = require('koa-logger');
+const KoaRouter = require('koa-router');
+const KoaStatic = require('koa-static');
 
 const app = new Koa();
 const router = new KoaRouter();
 
 app.use(KoaLogger());
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+app.use(KoaStatic('frontend/public'));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.listen(3000);
