85a361fa94d70cf6e88e1936b6ac11af69d1b8fe
akalenda <akalenda@users.noreply.github.com>
2019-04-28 09:06:32 -0700

    Created a mock, concrete database. Though really, it's just a class with some JSON pretending to be a database :-)

---
+++ src/backend/FakeDatabase.js
@@ -0,0 +1,44 @@
+const Cryptographer = require('./Cryptographer');
+
+const sampleSalt = Cryptographer.makeSalt();
+const tables = {
+    'passwordsByUser': {
+        'testuser': Cryptographer.encrypt('password1234', sampleSalt)
+    },
+    'saltsByUser': {
+        'testuser': sampleSalt
+    },
+    'usersBySalt': {}
+};
+tables.usersBySalt[sampleSalt] = 'testuser';
+
+function simulateQueryTime(milliseconds, resolve) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+/**
+ * For demo purposes.
+ *
+ * One protection that isn't on display in all of this, by the way, is
+ * in our backend having different "users" with different permissions:
+ * One that's used to create or update passwords, and one that reads
+ * passwords (for checking logins). This mitigates the damage a SQL
+ * injection might cause, by making it unlikely that malware can
+ * change passwords.
+ */
+
+class FakeDatabase {
+
+    /**
+     * @param {String} tableName
+     * @param {String} primaryKey
+     * @returns {Promise<*>}
+     */
+    apiGet(tableName, primaryKey) {
+        return simulateQueryTime(20, () => tables[tableName][primaryKey]);
+    }
+}
+
+module.exports = FakeDatabase;
