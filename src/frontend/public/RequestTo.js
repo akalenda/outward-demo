00ae8ed3756be0793bc3dee6d091e620b3306a00
akalenda <akalenda@users.noreply.github.com>
2019-04-27 21:18:06 -0700

    Imported koa-bodyparser. Otherwise, the data is buried deeply in the request. Yet, for some reason it's not parsing plain text; only JSON! Despite clearly seeing 'text/plain' in the header, I'm still getting an empty object from the parser. But it's working when I send JSON; that will do for now.

--- src/frontend/public/RequestTo.js
+++ src/frontend/public/RequestTo.js
@@ -5,7 +5,9 @@ class RequestTo {
      */
     constructor (uri) {
         this._uri = uri;
-        this._details = {};
+        this._details = {
+            headers: {}
+        };
     }
 
     /**
@@ -13,8 +15,20 @@ class RequestTo {
      * @returns {RequestTo}
      */
     sendingPlainText(text) {
-        this._details.contentType = HttpProtocols.CONTENT_TYPES.plainText;
-        this._details.body = text;
+        // TODO: Ugh, this hack! Figure out how to configure koa-bodyparser to parse the plaintext!
+        //this._details.headers["Content-Type"] = HttpProtocols.CONTENT_TYPES.plainText;
+        //this._details.body = text;
+        this.sendingJson({text: text});
+        return this;
+    }
+
+    /**
+     * @param {Object} obj
+     * @returns {RequestTo}
+     */
+    sendingJson(obj) {
+        this._details.headers["Content-Type"] = HttpProtocols.CONTENT_TYPES.json;
+        this._details.body = JSON.stringify(obj);
         return this;
     }
 
@@ -23,7 +37,8 @@ class RequestTo {
      */
     POST() {
         this._details.method = "POST";
-        let promisedResponse = fetch(this._uri, this._details);
+        let request = new Request(this._uri, this._details);
+        let promisedResponse = fetch(request);
         return new FutureResponse(promisedResponse);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const Koa = require('koa');
+const KoaBodyParser = require('koa-bodyparser');
 const KoaLogger = require('koa-logger');
 const KoaRouter = require('koa-router');
 const KoaStatic = require('koa-static');
+const MathExpression = require('./backend/MathExpression');
 
 const app = new Koa();
 const router = new KoaRouter();
 
 app.use(KoaLogger());
 app.use(KoaStatic('frontend/public'));
+app.use(KoaBodyParser());
 
-router.get('/api/math', (ctx, ignored) => ctx.body = MathExpression.from(ctx.body).evaluate().toString());
+router.post('/api/math', async (ctx, ignored) => {
+    let text = ctx.request.body.text;
+    ctx.body = MathExpression.from(text).evaluate().toString()
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
